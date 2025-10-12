36dd1d058f9c033608831274ec4822b421ea2d3e
johannesskagius <johannes@johannesskagius.com>
2025-10-12 07:16:35 +0200

    refactor(race_segment): Rename 'time' to 'splitTimeOfTotalRace' for clarity
    The `time` field in the `RaceSegment` class has been renamed to `splitTimeOfTotalRace`.
    
    This change makes the purpose of the field more explicit, indicating that it represents the split time relative to the total duration of the race.

--- lib/src/objects/race_segment.dart
+++ lib/src/objects/race_segment.dart
@@ -3,7 +3,7 @@ import 'package:swim_apps_shared/src/events/checkpoint.dart';
 /// Represents a single recorded moment in a race.
 class RaceSegment {
   final CheckPoint checkPoint;
-  final Duration time;
+  final Duration splitTimeOfTotalRace;
 
-  RaceSegment({required this.checkPoint, required this.time});
+  RaceSegment({required this.checkPoint, required this.splitTimeOfTotalRace});
 }
